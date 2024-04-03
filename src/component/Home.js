43743742d6587f21787bc3aab820fb71f72a8b3d
MercyAngelin2000 <mercyangelin2000@gmail.com>
2024-04-03 18:08:03 +0530

    whatsapp

--- src/component/Home.js
+++ src/component/Home.js
@@ -255,10 +255,12 @@ function Home() {
 
                                             </span>
                                             <span onClick={() => handleCartList(it)} role='button'>{it?.cart === true ? "Added to cart" : "Add to cart"}</span>
-                                            <span role='button'>
+                                            <span role='button' className='linkColor'>
+                                                <a href="https://wa.me/918778451528" target='_blank' rel="noreferrer">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply-fill" viewBox="0 0 16 16">
                                                     <path d="M5.921 11.9 1.353 8.62a.72.72 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
                                                 </svg>
+                                                </a>
                                             </span>
                                         </div>
                                         <div class="card-body text-center">
